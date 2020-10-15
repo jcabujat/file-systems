8e2ab8560ad215d9dc50183652a1f25797d31148
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 10:13:44 +0800

    03: Copying files using Files.copy

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -1,34 +1,17 @@
 package com.jcabujat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
-//        Path subPath = FileSystems.getDefault().getPath("file","SubdirectoryFile.txt");
-        Path subPath = Paths.get(".", "file", "SubdirectoryFile.txt");
-        // Paths.get(".") returns the absolute path same as FileSystems.getDefault()
-        printFile(subPath);
-        Path outPath = Paths.get("C:\\Users\\jonat\\IdeaProjects\\OutThere.txt.txt");
-        printFile(outPath);
-        Path path2 = FileSystems.getDefault().getPath(".", "file", "..", "file", "SubdirectoryFile.txt");
-        System.out.println(path2.normalize().toAbsolutePath()); // normalize() method to remove the dots in the path
-        printFile(path2);
-    }
-
-    private static void printFile(Path path) {
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+        try {
+            Path sourcePath = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyPath = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Files.copy(sourcePath, copyPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
