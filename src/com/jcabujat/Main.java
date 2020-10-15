a22527d479b5d41e01debc4aa5e7b09c37360fce
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 11:20:48 +0800

    04: Codes for getting  basic file attributes such as file size, last modified date, creation date, etc..

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -4,11 +4,22 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
+//            // Create a blank file
+////            Path fileToCreate = FileSystems.getDefault().getPath("Examples","file2.txt");
+////            Files.createFile(fileToCreate);
+//
+//            // Create a directory
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2\\Dir3\\Dir4\\Dir5\\Dir6");
+////            Files.createDirectory(dirToCreate);
+//            Files.createDirectories(dirToCreate); // does not throw exception if the directory to be created already exists
+
+
 //            //Copy file
 //            Path sourcePath = FileSystems.getDefault().getPath("Examples", "file1.txt");
 //            Path copyPath = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
@@ -29,10 +40,23 @@ public class Main {
 //            Path destination = FileSystems.getDefault().getPath("Examples", "file1 rename.txt");
 //            Files.move(fileToMove, destination, StandardCopyOption.REPLACE_EXISTING);
 
-            // Delete file
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples","Dir1", "file1copy.txt");
-//            Files.delete(fileToDelete); // throws exception when file is not existing
-            Files.deleteIfExists(fileToDelete); // doesn't throw an exception when file is not existing
+//            // Delete file
+//            Path fileToDelete = FileSystems.getDefault().getPath("Examples","Dir1", "file1copy.txt");
+////            Files.delete(fileToDelete); // throws exception when file is not existing
+//            Files.deleteIfExists(fileToDelete); // doesn't throw an exception when file is not existing
+            // Get file size
+            Path file1 = FileSystems.getDefault().getPath("Examples\\Dir1\\file1.txt");
+            long size = Files.size(file1);
+            System.out.println("file1 size = " + size);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(file1));
+
+            BasicFileAttributes attributes = Files.readAttributes(file1, BasicFileAttributes.class);
+            System.out.println("Size = " + attributes.size());
+            System.out.println("Last modified = " + attributes.lastModifiedTime());
+            System.out.println("Created time = " + attributes.creationTime());
+            System.out.println("Is directory = " + attributes.isDirectory());
+            System.out.println("Is regular file = " + attributes.isRegularFile());
+
 
         } catch (IOException e) {
             e.printStackTrace();
