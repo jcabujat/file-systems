9c112ff424e30de5740e94ddb6b36fc415256ba2
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 10:36:40 +0800

    04: Codes for copying, moving, renaming and deleting files or directories.

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -9,9 +9,31 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Path sourcePath = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyPath = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourcePath, copyPath);
+//            //Copy file
+//            Path sourcePath = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyPath = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourcePath, copyPath, StandardCopyOption.REPLACE_EXISTING);
+//
+//            // Copy directory
+//            sourcePath = FileSystems.getDefault().getPath("Examples","Dir1");
+//            copyPath = FileSystems.getDefault().getPath("Examples","Dir4");
+//            Files.copy(sourcePath, copyPath, StandardCopyOption.REPLACE_EXISTING);
+
+//            // Move file
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples","file1copy.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1","file1copy.txt");
+//            Files.move(fileToMove, destination, StandardCopyOption.REPLACE_EXISTING);
+
+//            // Rename file - directory should be the same, you only need to change file name then use Files.move method
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples","file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1 rename.txt");
+//            Files.move(fileToMove, destination, StandardCopyOption.REPLACE_EXISTING);
+
+            // Delete file
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples","Dir1", "file1copy.txt");
+//            Files.delete(fileToDelete); // throws exception when file is not existing
+            Files.deleteIfExists(fileToDelete); // doesn't throw an exception when file is not existing
+
         } catch (IOException e) {
             e.printStackTrace();
         }
